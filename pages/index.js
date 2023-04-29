aa8c6ca09d08fa62457e21dbc6ebf3e5bd867ea5
davidnx01 <info@webdavid.sk>
2023-04-29 11:58:25 +0200

    globals.css preparation, basic responsive layout

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
+import About from "@/common/Containers/About";
+import Agents from "@/common/Containers/Agents";
+import BestOffers from "@/common/Containers/BestOffers";
+import Footer from "@/common/Containers/Footer";
 import Hero from "@/common/Containers/Hero";
+import Services from "@/common/Containers/Services";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+    <div id="wrapper">
+      <div className="header">
+        <Hero />
+      </div>
       <BestOffers />
       <About />
       <Services />
       <Agents />
       <Footer />
+    </div> 
     </>
   )
 }
