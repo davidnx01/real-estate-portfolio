c2962dd60a3c14258a71126679c01f03a04919fb
davidnx01 <info@webdavid.sk>
2023-05-01 13:04:49 +0200

    Services Design

---
+++ common/Components/Service.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styles from "../../styles/service.module.css"
+import { services } from '../Constants/serviceConts'
+import { HiArrowLongRight } from 'react-icons/hi2'
+
+const Service = () => {
+  return (
+    <>
+        {services.map((service) => (
+            <div className={styles.service}>
+                <div className={styles.icon}>
+                    <img src={service.icon} alt="Service Icon" />
+                </div>
+
+                <h4>{service.title}</h4>
+
+                <p>{service.description}</p>
+
+                <a className={styles.service_cta}>
+                    <HiArrowLongRight size={35} />
+                    Zistiť Viac
+                </a>
+            </div>
+        ))}
+    
+    </>
+  )
+}
+
+export default Service
--- common/Containers/Services.jsx
+++ common/Containers/Services.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import "../../styles/services.module.css"
+import styles from "../../styles/services.module.css"
+import Heading from '../Components/Heading'
+import Service from '../Components/Service'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <>
+      <section id='services' className={styles.services_section}>
+        <Heading 
+          title="Naše Zameranie"
+          description="Sme skupina skúsených profesionálov, ktorí sú oddaní tomu,
+          aby sme Vám pomohli nájsť ideálne bývanie alebo obchodnú nehnuteľnosť."
+        />
+
+        <div className={styles.services_wrap}>
+          <Service />
+        </div>
+
+
+      </section>
+    </>
   )
 }
 
