c73be55d2d31d11bcbee7d4459f00f7864ed60eb
davidnx01 <info@webdavid.sk>
2023-05-01 12:46:22 +0200

    Service Constants

---
+++ common/Constants/serviceConts.js
@@ -0,0 +1,17 @@
+export const services = [
+    {
+        icon: "/buy.svg",
+        title: "Kúpiť Dom",
+        description: "Pomôžeme Vám nájsť ideálny dom, ktorý bude splňovať Vaše potreby a predstavy. S našimi skúsenými realitnými maklérmi Vám dokážeme ponúknuť najlepšie možné možnosti na trhu."
+    },
+    {
+        icon: "/rent.svg",
+        title: "Prenajať Dom",
+        description: "Pomôžeme nájsť ten správny dom na prenájom. Naši profesionálni realitní makléri Vám poskytnú všetky potrebné informácie o nehnuteľnostiach, ktoré sú momentálne k dispozícii."
+    },
+    {
+        icon: "/sale.svg",
+        title: "Predať Dom",
+        description: "Naši skúsení realitní makléri pomôžu s každým krokom predaja vášho domu, vrátane stanovenia ceny, fotografovania a prezentácie, aby ste dosiahli najlepší výsledok."
+    },
+]
