073cbb88cc2bb80e953857547968150abefb40b1
davidnx01 <info@webdavid.sk>
2023-05-01 16:05:39 +0200

    Agents Layout

--- common/Containers/Agents.jsx
+++ common/Containers/Agents.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from "../../styles/agents.module.css"
 import Heading from '../Components/Heading'
+import { agentsConts } from '../Constants/agentsConts'
+import { RiPhoneFill, RiInstagramFill } from "react-icons/ri"
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa"
 
 const Agents = () => {
   return (
@@ -12,9 +15,36 @@ const Agents = () => {
           ktorí Vám pomôžu s kúpou, predajom alebo prenájmom nehnuteľnosti."
         />
 
+        <div className={styles.agents_wrap}>
+          {agentsConts.map((agent) => (
+            <div className={styles.agent_box}>
+              <div className={styles.agent_upper_part}>
+                <div className={styles.callCTA}>
+                  <RiPhoneFill />
+                </div>
+                <img src={agent.image} alt="Agent" />
+                <div className={styles.experience}>
+                  <p>{agent.experience}</p>
+                </div>
+              </div>
 
 
+              <div className={styles.agent_bottom_part}>
+                <h4>{agent.name}</h4>
+                <div className={styles.agent_bottom_part_row}>
+                  <p>{agent.job}</p>
+                </div>
 
+                <div className={styles.agent_socials}>
+                  <FaFacebookF />
+                  <FaLinkedinIn />
+                  <RiInstagramFill />
+                  <FaTwitter />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </section>
     </>
   )
