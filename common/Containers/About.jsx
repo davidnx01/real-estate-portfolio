685fa2484599357d2b94a776e23390c09f5c80c4
davidnx01 <info@webdavid.sk>
2023-05-01 12:00:13 +0200

    About Left Side Design

--- common/Containers/About.jsx
+++ common/Containers/About.jsx
@@ -3,6 +3,7 @@ import "../../styles/about.module.css"
 import Heading from '../Components/Heading'
 import Benefit from '../Components/Benefit'
 import styles from "../../styles/about.module.css"
+import { HiArrowLongRight } from "react-icons/hi2"
 
 const About = () => {
   return (
@@ -18,6 +19,11 @@ const About = () => {
         <div className={styles.about_left_side_benefits}>
           <Benefit />
         </div>
+
+        <a href="#" className={styles.about_cta}>
+          <HiArrowLongRight size={35} />
+          Zistiť Viac
+        </a>
       </div>
 
 
