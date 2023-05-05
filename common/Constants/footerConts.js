1fce70ad660f7427428f474fab22bedc52157e64
davidnx01 <info@webdavid.sk>
2023-05-05 20:20:17 +0200

    Footer content final

--- common/Constants/footerConts.js
+++ common/Constants/footerConts.js
@@ -1,26 +1,36 @@
-import { FaPhoneAlt } from "react-icons/fa";
+import { BsEnvelopeOpen } from "react-icons/bs";
+import { FaPhoneAlt, FaTwitter } from "react-icons/fa";
 import { MdOutlineWifi } from "react-icons/md";
+import { RiFacebookFill, RiInstagramFill } from "react-icons/ri";
+import { RxLinkedinLogo } from "react-icons/rx";
 
 export const footerContent = [
     {
         linkLink: "Domov",
         idLink: "home",
         linkContact: <FaPhoneAlt /> + "+421 919 999 999",
-        linkIcon: <RiFacebookFill />
+        linkIcon: <RiFacebookFill />,
+        idIcon: "www.facebook.sk"
     },
     {
         linkLink: "Najlepšie ponuky",
         idLink: "bestOffers",
         linkContact: <MdOutlineWifi /> + "www.webdavid.sk",
+        linkIcon: <RxLinkedinLogo />,
+        idIcon: "www.linkedin.com"
     },
     {
         linkLink: "O Nás",
         idLink: "aboutus",
         linkContact: <BsEnvelopeOpen /> + "info@webdavid.sk",
+        linkIcon: <RiInstagramFill />,
+        idIcon: "www.instagram.com"
     },
     {
         linkLink: "Agenti",
-        idLink: "agents"
+        idLink: "agents",
+        linkIcon: <FaTwitter />,
+        idIcon: "www.twitter.com"
     },
     {
         linkLink: "Kontakt",
