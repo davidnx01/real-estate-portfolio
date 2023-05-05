f1c3acd302d2b502ebd247c517cb80b1d7f73764
davidnx01 <info@webdavid.sk>
2023-05-05 20:05:12 +0200

    Footer content 1

---
+++ common/Constants/footerConts.js
@@ -0,0 +1,22 @@
+export const footerContent = [
+    {
+        link: "Domov",
+        id: "home"
+    },
+    {
+        link: "Najlepšie ponuky",
+        id: "bestOffers"
+    },
+    {
+        link: "O Nás",
+        id: "aboutus"
+    },
+    {
+        link: "Agenti",
+        id: "agents"
+    },
+    {
+        link: "Kontakt",
+        id: "contact"
+    },
+]
